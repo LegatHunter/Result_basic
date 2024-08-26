38e0797dd2abb9e2a0ed7e700f7c9f4b7eed1530
Nikita Prebyshevskiy <legathunter@mail.ru>
2024-08-26 17:55:31 +0300

    good

--- Теоретическая база JavaScript, часть 3/script.js
+++ Теоретическая база JavaScript, часть 3/script.js
@@ -1,16 +1,14 @@
 // Задание #1
-
 // Напишите функцию sumNumbers(), которая принимает произвольное количество чисел через запятую и возвращает их сумму.
-
 // const result1 = sumNumbers(1, 2, 3, 4, 5);
 // console.log(result1); // 15
-
 // const result2 = sumNumbers(10, 20, 30);
 // console.log(result2); // 60
+
+
+
 // Задание #2
-
 // Напишите функцию findMaxValue(), которая принимает массив чисел и находит максимальное значение. Если массив пустой, то возвращается значение undefined.
-
 // const numbers1 = [1, 2, 3, 4, 5];
 // const max1 = findMaxValue(numbers1);
 // console.log(max1); // 5
@@ -22,8 +20,10 @@
 // const emptyArray = [];
 // const maxEmpty = findMaxValue(emptyArray);
 // console.log(maxEmpty); // undefined
-// Задание #3
 
+
+
+// Задание #3
 // Напишите функцию calculateAverage(), которая принимает массив чисел и вычисляет их среднее арифметическое. Если массив пустой, то возвращается 0.
 
 // const numbers1 = [1, 2, 3, 4, 5];
@@ -34,15 +34,19 @@
 
 // const emptyArray = [];
 // console.log(calculateAverage(emptyArray)); // 0
-// Задание #4
 
+
+
+// Задание #4
 // Напишите функцию isPalindrome(), которая принимает строку и возвращает true, если она является палиндромом, иначе — false.
 
 // console.log(isPalindrome('level')); // true
 // console.log(isPalindrome('radar')); // true
 // console.log(isPalindrome('hello')); // false
-// Задание #5
 
+
+
+// Задание #5
 // Напишите функцию removeDuplicates(), которая принимает массив и возвращает новый массив без дубликатов.
 
 // const numbers1 = [1, 2, 3, 3, 4, 5, 5];
@@ -53,8 +57,10 @@
 
 // const emptyArray = [];
 // console.log(removeDuplicates(emptyArray));  // []
-// Задание #6
 
+
+
+// Задание #6
 // Создайте функцию createCounter(), которая принимает начальное значение и возвращает три функции:
 
 // inc() — увеличивает значение на 1;
